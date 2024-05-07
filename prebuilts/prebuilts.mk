5b7a2a87a54b3ded38c63beb2919da4f649600c4
chaitanyakm <chaitanyaarora3@gmail.com>
2024-05-07 21:03:18 +0530

    Version: 1.62

--- prebuilts/prebuilts.mk
+++ prebuilts/prebuilts.mk
@@ -1,5 +1,6 @@
 # Copy permission files
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bcr/prebuilts/product/etc/permissions,$(TARGET_COPY_OUT_PRODUCT)/etc/permissions)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bcr/prebuilts/product/etc/sysconfig,$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig)
 
 # Build apps
 PRODUCT_PACKAGES += \
