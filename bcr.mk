59a77a518f25d13cb7404bcf58cf0853a572a819
chaitanyakm <chaitanyaarora3@gmail.com>
2025-04-30 19:52:19 +0000

    bcr: add vendor/bcr to PRODUCT_SOONG_NAMESPACES

--- bcr.mk
+++ bcr.mk
@@ -4,3 +4,6 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 # Include prebuilts packages
 include vendor/bcr/prebuilts/prebuilts.mk
 RELAX_USES_LIBRARY_CHECK=true
+
+# Soong Namespace
+PRODUCT_SOONG_NAMESPACES += vendor/bcr
